0ae08c21412dc50094acf76fae0a1f5472c81cc9
ylem76 <ylem76@gmail.com>
2024-07-27 14:38:42 +0900

    #1 eslint setting

---
+++ src/store/authSampleStore.ts
@@ -0,0 +1,33 @@
+import { create } from 'zustand';
+
+// authState 타입 정의
+interface AuthState {
+  isAuthenticated: boolean;
+  user: { username: string } | null;
+  login: (username: string, password: string) => void;
+  logout: () => void;
+}
+
+// store 생성
+const useAuthStore = create<AuthState>(set => ({
+  // store내부 기본값 정의
+  isAuthenticated: false,
+  user: null,
+
+  // store에서 사용할 기본 메서드 정의
+  login: (username, password) => {
+    if (username === 'user' && password === 'password') {
+      set(state => ({
+        isAuthenticated: true,
+        user: { username: '유저' },
+      }));
+    } else {
+      alert('로그인 실패');
+    }
+  },
+
+  logout: () => set({ isAuthenticated: false, user: null }),
+}));
+
+// 작성한 store export 외부에서 hook으로 사용
+export default useAuthStore;
