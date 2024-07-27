0802dad9f74532e78409b490856dc39e97c0cae3
ylem76 <ylem76@gmail.com>
2024-07-27 15:05:17 +0900

    #3 zustand 샘플 코드

--- src/store/authSampleStore.ts
+++ src/store/authSampleStore.ts
@@ -1,11 +1,11 @@
-import { create } from 'zustand';
+import { create } from 'zustand'
 
 // authState 타입 정의
 interface AuthState {
-  isAuthenticated: boolean;
-  user: { username: string } | null;
-  login: (username: string, password: string) => void;
-  logout: () => void;
+  isAuthenticated: boolean
+  user: { username: string } | null
+  login: (username: string, password: string) => void
+  logout: () => void
 }
 
 // store 생성
@@ -16,18 +16,19 @@ const useAuthStore = create<AuthState>(set => ({
 
   // store에서 사용할 기본 메서드 정의
   login: (username, password) => {
+    // 임시로 아래 정보로 로그인 했을 때 로그인 처리를 합니다.
     if (username === 'user' && password === 'password') {
-      set(state => ({
+      set(() => ({
         isAuthenticated: true,
         user: { username: '유저' },
-      }));
+      }))
     } else {
-      alert('로그인 실패');
+      alert('로그인 실패')
     }
   },
 
   logout: () => set({ isAuthenticated: false, user: null }),
-}));
+}))
 
 // 작성한 store export 외부에서 hook으로 사용
-export default useAuthStore;
+export default useAuthStore
