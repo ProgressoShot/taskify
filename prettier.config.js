34feaac62a724d0c1b1f3fd841faa0663c673fee
ylem76 <ylem76@gmail.com>
2024-07-27 14:37:51 +0900

    chore/#1 eslint setting

--- prettier.config.js
+++ prettier.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  semi: true,
+  semi: false,
   singleQuote: true,
+  jsxSingleQuote: true,
   printWidth: 80,
-  trailingComma: "es5",
+  trailingComma: 'es5',
   tabWidth: 2,
   useTabs: false,
   bracketSpacing: true,
-  arrowParens: "avoid",
-  plugins: ["prettier-plugin-tailwindcss"],
+  arrowParens: 'avoid',
+  plugins: ['prettier-plugin-tailwindcss'],
 };
