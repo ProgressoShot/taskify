4cce4cc6cc728acba4a60afda7a8fb3e67f3ab42
KingNono1030 <hoejy0821@gmail.com>
2024-07-30 19:17:40 +0900

    [#10] set up custom colors in tailwind.css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
@@ -13,8 +13,31 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        custom: {
+          violet: '#5534DA',
+          red: '#D6173A',
+          green: '#7AC555',
+          purple: '#760DDE',
+          orange: '#FFA500',
+          blue: '#76A5EA',
+          pink: '#E876EA',
+          black: {
+            100: '#4B4B4B',
+            200: '#333236',
+            300: '#171717',
+          },
+          gray: {
+            100: '#FAFAFA',
+            200: '#EEEEEE',
+            300: '#D9D9D9',
+            400: '#9FA6B2',
+            500: '#787486',
+          },
+        },
+      },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
