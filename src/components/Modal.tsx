436f3fafb6e6253737a0ba29a19c85358549e231
KingNono1030 <hoejy0821@gmail.com>
2024-07-31 23:03:44 +0900

    [#13] ✨ Add Confirm type of Modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,8 @@
 import cn from 'classnames'
 import { ReactNode } from 'react'
 
+import Button from '@/components/Button'
+
 interface ModalProps {
   className?: string
   children: ReactNode
@@ -14,10 +16,10 @@ export default function Modal({ children }: ModalProps) {
   )
 }
 
-function Alert({ children, className }: ModalProps) {
+function Confirm({ children, className }: ModalProps) {
   const alertClass = cn(className)
   return (
-    <div className='flex w-full items-center justify-center rounded-lg bg-white px-16 py-10 md:w-auto'>
+    <div className='flex flex-col items-center justify-center gap-8 rounded-lg bg-white px-10 py-8 md:px-16 md:py-10'>
       {children}
     </div>
   )
@@ -26,11 +28,21 @@ function Alert({ children, className }: ModalProps) {
 function Message({ children, className }: ModalProps) {
   const messageClass = cn(className)
   return (
-    <h2 className='text-lg font-medium text-custom-black-200 md:text-xl'>
+    <h2 className='text-base font-medium text-custom-black-200 md:text-xl'>
       {children}
     </h2>
   )
 }
 
+function ConfirmButton({ children, className }: ModalProps) {
+  const buttonClass = cn(className)
+  return (
+    <Button className='h-[42px] w-48 text-sm font-semibold md:h-12 md:w-60 md:text-base'>
+      {children}
+    </Button>
+  )
+}
+
+Modal.Confirm = Confirm
 Modal.Message = Message
-Modal.Alert = Alert
+Modal.ConfirmButton = ConfirmButton
