162f79aab71fde1ce9a42ffec4033da221f316b3
JuhyeokC <bos2ablues@gmail.com>
2024-08-01 17:32:16 +0900

    [#20] 🎨 Add responsive & Separate logo components

--- src/layouts/RootHeader.tsx
+++ src/layouts/RootHeader.tsx
@@ -1,28 +1,22 @@
-import Link from 'next/link'
-
-import LogoIcon from '@/assets/logo.svg'
-import LogoSymbol from '@/assets/logo-symbol.svg'
+import styles from './ResponsiveLayout.module.css'
+import RootLogo from './RootLogo'
 
 type HeaderContextProps = {
-  size?: 'large' | 'medium' | 'small'
   theme?: 'dark' | 'light'
   border?: boolean
 }
 
-type HeaderProps = HeaderContextProps & { children?: any }
+export type HeaderProps = HeaderContextProps & { children?: any }
 
 function RootHeader({ theme, border, children }: HeaderProps) {
   return (
     <header
-      className={`grid h-[70px] w-screen ${theme === 'dark' ? '#000000' : 'bg-[#ffffff]'}`}
-      style={{
-        gridTemplateColumns: '300px auto',
-      }}
+      className={`grid h-[70px] w-screen ${styles['ResponsiveLayoutLeft']} ${theme === 'dark' ? '#000000' : 'bg-[#ffffff]'}`}
     >
       <div
-        className={`h-full w-full overflow-hidden px-6 py-4 ${border ? 'border-r-[1px] border-[#d9d9d9]' : ''}`}
+        className={`h-full w-full overflow-hidden px-4 py-4 md:px-6 ${border ? 'border-r-[1px] border-[#d9d9d9]' : ''}`}
       >
-        <LogoSection theme={theme} />
+        <RootLogo theme={theme} />
       </div>
       <div
         className={`h-full w-full overflow-hidden px-10 py-4 ${border ? 'border-b-[1px] border-[#d9d9d9]' : ''}`}
@@ -32,22 +26,6 @@ function RootHeader({ theme, border, children }: HeaderProps) {
         </section>
       </div>
     </header>
-  )
-}
-
-/**
- * @todo 반응형 작업: 모바일일 때 LogoSymbol
- */
-function LogoSection({ theme }: HeaderProps) {
-  const fill = theme === 'dark' ? '#ffffff' : '#5534DA'
-
-  return (
-    <section className='flex h-full w-full items-center'>
-      <Link href={'/'}>
-        <LogoIcon fill={fill} />
-        {/* <LogoSymbol fill={fill} /> */}
-      </Link>
-    </section>
   )
 }
 
