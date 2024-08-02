f9b8f6110863c4086458478f0efb0a71a49b62a6
JiinSeok <seokjiin1073@gmail.com>
2024-08-02 22:18:57 +0900

    [#43] 미사용 임포트문 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 'use client'
-import Image from 'next/image'
 import Link from 'next/link'
 import { useState } from 'react'
 
-import Button from '@/components/Button'
 import RootHeader from '@/layouts/RootHeader'
 import useAuthStore from '@/store/authSampleStore'
 
@@ -23,14 +21,17 @@ export default function LandingPage() {
       <RootHeader theme='dark'>
         <RootHeader.Features theme='dark'>
           <div className='flex gap-9'>
-            <Link href={'/auth/login'}>로그인</Link>
-            <Link href={'/auth/sign'}>회원가입</Link>
+            <Link href={'/login'}>로그인</Link>
+            <Link href={'/sign'}>회원가입</Link>
           </div>
         </RootHeader.Features>
       </RootHeader>
       <main className='flex min-h-screen flex-col items-center justify-between p-24'>
         {isAuthenticated ? `is login - ${user?.username}` : 'is logout'}
 
+        <p>
+          id:user, pw:password를 입력하면 zustand의 store데이터가 변경됩니다
+        </p>
         <p>
           id:user, pw:password를 입력하면 zustand의 store데이터가 변경됩니다
         </p>
