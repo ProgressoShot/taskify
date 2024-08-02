1b20f4473e3202fc9456ee6b0f84851b90b4b3a9
KingNono1030 <hoejy0821@gmail.com>
2024-08-02 19:48:41 +0900

    [#13] 🔥 모달 리셋

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -6,43 +6,14 @@ import Button from '@/components/Button'
 interface ModalProps {
   className?: string
   children: ReactNode
+  onClick?: () => void
 }
 
-export default function Modal({ children }: ModalProps) {
+export default function Modal({ children, className }: ModalProps) {
+  const modalClass = cn('px-4 py-6 rounded-lg bg-white md:px-6', className)
   return (
     <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-70 px-6'>
-      {children}
+      <div className={modalClass}>{children}</div>
     </div>
   )
 }
-
-function Confirm({ children, className }: ModalProps) {
-  const alertClass = cn(className)
-  return (
-    <div className='flex flex-col items-center justify-center gap-8 rounded-lg bg-white px-10 py-8 md:px-16 md:py-10'>
-      {children}
-    </div>
-  )
-}
-
-function Message({ children, className }: ModalProps) {
-  const messageClass = cn(className)
-  return (
-    <h2 className='text-base font-medium text-custom-black-200 md:text-xl'>
-      {children}
-    </h2>
-  )
-}
-
-function ConfirmButton({ children, className }: ModalProps) {
-  const buttonClass = cn(className)
-  return (
-    <Button className='h-[42px] w-48 text-sm font-semibold md:h-12 md:w-60 md:text-base'>
-      {children}
-    </Button>
-  )
-}
-
-Modal.Confirm = Confirm
-Modal.Message = Message
-Modal.ConfirmButton = ConfirmButton
