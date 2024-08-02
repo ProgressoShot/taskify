6dfad67f6859d4c965089a1d84f2994b0641c300
JuhyeokC <bos2ablues@gmail.com>
2024-08-02 17:27:38 +0900

    [#28] ✨ 대시보드 카드 컴포넌트 추가

--- src/app/myDashboard/page.tsx
+++ src/app/myDashboard/page.tsx
@@ -1,5 +1,55 @@
-import DashboardLayout from '@/layouts/DashboardLayout'
+import { DashboardName, DashboardNameProps } from '@/components/DashboardName'
+import DashboardLayout, { ChildrenProp } from '@/layouts/DashboardLayout'
 import RootHeader from '@/layouts/RootHeader'
+
+type DashboardInfo = {
+  id: number
+  title: string
+  color: string
+  createdAt: Date
+  updatedAt: Date
+  createdByMe: Boolean
+  userId: number
+}
+
+interface DashboardCardProps extends DashboardNameProps {
+  onClick?: () => void
+}
+
+function DashboardCard({ type, children, onClick }: DashboardCardProps) {
+  const classNames =
+    type === 'side'
+      ? 'py-3 px-4'
+      : 'round-container h-full w-full border border-custom-gray-300 bg-white p-5'
+
+  return (
+    <button
+      onClick={onClick}
+      className={`rounded-lg transition hover:bg-custom-gray-100 ${classNames}`}
+    >
+      <DashboardName type={type}>{children}</DashboardName>
+    </button>
+  )
+}
+
+function DashboardCardList({ children }: ChildrenProp) {
+  return (
+    <section
+      className='grid gap-4'
+      style={{
+        gridTemplateColumns: 'repeat(3, 1fr)',
+      }}
+    >
+      <DashboardCard type='add'>새로운 대시보드</DashboardCard>
+      {children}
+    </section>
+  )
+}
+
+const DASHBOARD_TEMP_ARRAY = Array.from(
+  { length: 5 },
+  (_, index) => `대시보드-${index + 1}`
+)
 
 export default function MyDashboard() {
   return (
@@ -29,7 +79,13 @@ export default function MyDashboard() {
            * @todo
            * 사이드 컴포넌트 구현 필요
            */}
-          aside
+          {DASHBOARD_TEMP_ARRAY.map((item, index) => {
+            return (
+              <DashboardCard type='side' key={`dashboard-side-${index}`}>
+                {item}
+              </DashboardCard>
+            )
+          })}
         </DashboardLayout.Sidebar>
         <DashboardLayout.Content>
           {/**
@@ -37,7 +93,15 @@ export default function MyDashboard() {
            * @todo
            * 나의 대시보드 컴포넌트 구현 필요
            */}
-          content
+          <DashboardCardList>
+            {DASHBOARD_TEMP_ARRAY.map((item, index) => {
+              return (
+                <DashboardCard type='card' key={`dashboard-card-${index}`}>
+                  {item}
+                </DashboardCard>
+              )
+            })}
+          </DashboardCardList>
         </DashboardLayout.Content>
       </DashboardLayout.Container>
     </DashboardLayout>
