ee80891ed378ecfbcabdddfbd0055e8354846617
JuhyeokC <bos2ablues@gmail.com>
2024-08-02 23:56:56 +0900

    [#31] 🎨 로고, 사이드바 'use client' 방식 수정

--- src/layouts/RootLogo.tsx
+++ src/layouts/RootLogo.tsx
@@ -1,25 +1,18 @@
-'use client'
-
 import Link from 'next/link'
 
 import LogoIcon from '@/assets/logo.svg'
 import LogoSymbol from '@/assets/logo-symbol.svg'
-import useMediaQuery from '@/hooks/useMediaQuery'
 
 import { HeaderProps } from './RootHeader'
 
 export default function RootLogo({ theme }: HeaderProps) {
-  const mode = useMediaQuery()
   const fill = theme === 'dark' ? '#ffffff' : '#5534DA'
 
   return (
     <section className='flex h-full w-full items-center justify-center md:justify-start'>
       <Link href={'/'}>
-        {mode === 'mobile' ? (
-          <LogoSymbol fill={fill} />
-        ) : (
-          <LogoIcon fill={fill} />
-        )}
+        <LogoSymbol fill={fill} className='block md:hidden' />
+        <LogoIcon fill={fill} className='hidden md:block' />
       </Link>
     </section>
   )
--- src/layouts/RootSidebar.tsx
+++ src/layouts/RootSidebar.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import AddSquareIcon from '@/assets/add-square.svg'
 
 import { ChildrenProp } from './DashboardLayout'
