d05c29547ad01c4abab1c5c5bf92611d57c01034
KingNono1030 <hoejy0821@gmail.com>
2024-08-05 06:50:51 +0900

    [#13] 🔥 모달에 기본 패딩 제거

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -10,7 +10,7 @@ interface ModalProps {
 }
 
 export default function Modal({ children, className }: ModalProps) {
-  const modalClass = cn('px-4 py-6 rounded-lg bg-white md:px-6', className)
+  const modalClass = cn('rounded-lg bg-white', className)
   return (
     <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-70 px-6'>
       <div className={modalClass}>{children}</div>
