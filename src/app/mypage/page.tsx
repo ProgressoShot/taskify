dadb2d12add0a77edeb749842f94f7b9f5d25c10
JiinSeok <seokjiin1073@gmail.com>
2024-08-05 22:14:49 +0900

    [#46] 🐛 임포트 관련 빌드 에러 해결

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,5 +1,28 @@
+import { Link } from 'next/navigation'
+
+import DashboardLayout from '@/layouts/DashboardLayout'
+import RootHeader from '@/layouts/RootHeader'
+
 export default function MyPagePage() {
   return (
-    <></>
-  );
+    <DashboardLayout>
+      <RootHeader border>
+        <RootHeader.Title>계정 관리</RootHeader.Title>
+        <RootHeader.Features>
+          <div className='flex gap-2 border-l-[1px] border-[#d9d9d9] px-6'>
+            <p>아바타</p>
+            <span>이름</span>
+          </div>
+        </RootHeader.Features>
+      </RootHeader>
+      <DashboardLayout.Container>
+        <DashboardLayout.Sidebar>aside</DashboardLayout.Sidebar>
+        <DashboardLayout.Content>
+          <Link hr>돌아가기</Link>
+          <div>프로필 Modal</div>
+          <div>비밀번호 변경 Modal</div>
+        </DashboardLayout.Content>
+      </DashboardLayout.Container>
+    </DashboardLayout>
+  )
 }
