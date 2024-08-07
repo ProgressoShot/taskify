d804db2bd475bda15189ff75d7106a3ee1a41e2d
JiinSeok <seokjiin1073@gmail.com>
2024-08-07 20:36:55 +0900

    [#39] SentInvitation 컴포넌트 파일 생성

---
+++ src/app/dashboard/[id]/edit/components/SentInvitation.tsx
@@ -0,0 +1,119 @@
+export function SentInvitation() {
+  return <></>
+}
+
+import cn from 'classnames'
+import { PropsWithChildren } from 'react'
+
+import Button from '@/components/Button'
+
+interface ReceivedInvitationProps {
+  dashboardTitle: string
+  inviter: string
+  inviteAccepted: boolean
+}
+
+const classNames = {
+  inner: {
+    default: 'px-4 md:px-8 lg:px-16 md:grid md:grid-cols-3',
+    item: 'border-b border-b-custom-gray-200 py-3.5',
+    mobile: 'flex w-full flex-col items-center',
+  },
+  cols: {
+    default: 'grid w-full md:flex',
+    style: {
+      gridTemplateColumns: '64px auto',
+    },
+  },
+  label: {
+    default: 'px-2 text-custom-gray-400 text-nowrap',
+    mobile: 'block w-16 flex-none md:hidden',
+  },
+  value: {
+    default:
+      'px-2 text-custom-black-200 text-nowrap overflow-hidden text-ellipsis',
+  },
+  button: {
+    default: 'w-full rounded-[4px] md:w-20',
+    area: 'mt-3.5 grid w-full flex-none grid-cols-2 gap-2.5 md:flex md:flex-auto',
+  },
+}
+
+function Item({ dashboardTitle, inviter, inviteAccepted }: ReceivedInvitationProps) {
+  return (
+    <div
+      className={cn(
+        classNames.inner.default,
+        classNames.inner.item,
+        classNames.inner.mobile
+      )}
+    >
+      <div
+        className={cn(classNames.cols.default)}
+        style={classNames.cols.style}
+      >
+        <p
+          className={cn(
+            classNames.label.default,
+            classNames.label.mobile,
+            'text-sm'
+          )}
+        >
+          이름
+        </p>
+        <p className={cn(classNames.value.default)}>{dashboardTitle}</p>
+      </div>
+      <div
+        className={cn(classNames.cols.default)}
+        style={classNames.cols.style}
+      >
+        <p
+          className={cn(
+            classNames.label.default,
+            classNames.label.mobile,
+            'text-sm'
+          )}
+        >
+          초대자
+        </p>
+        <p className={cn(classNames.value.default)}>{inviter}</p>
+      </div>
+      <div
+        className={cn(
+          classNames.value.default,
+          'mt-3.5 grid w-full flex-none grid-cols-2 gap-2.5 md:m-auto md:flex md:flex-auto'
+        )}
+      >
+        <Button
+          className={cn(classNames.button.default)}
+          color={inviteAccepted ? 'primary' : 'secondary'}
+        >
+          수락
+        </Button>
+        <Button
+          className={cn(classNames.button.default)}
+          color={!inviteAccepted ? 'primary' : 'secondary'}
+        >
+          거절
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+function ReceivedInvitation({ children }: PropsWithChildren) {
+  return (
+    <div className='pb-8'>
+      <div className={cn(classNames.inner.default, 'hidden')}>
+        <p className={cn(classNames.label.default, 'text-base')}>이름</p>
+        <p className={cn(classNames.label.default, 'text-base')}>초대자</p>
+        <p className={cn(classNames.label.default, 'text-base')}>수락여부</p>
+      </div>
+      {children}
+    </div>
+  )
+}
+
+ReceivedInvitation.Item = Item
+
+export default ReceivedInvitation
