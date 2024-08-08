4b7620a9406ceb9107a16501f3d612a4429fb241
JiinSeok <seokjiin1073@gmail.com>
2024-08-08 16:45:37 +0900

    [#39] 레이아웃에 대시보드 이름 추가

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,8 +1,9 @@
 'use client'
 
-import { useParams } from 'next/navigation'
-import { PropsWithChildren } from 'react'
+import {useParams} from 'next/navigation'
+import {PropsWithChildren} from 'react'
 
+import {getDashboardInfoById} from '@/app/utils/api'
 import DashboardLayout from '@/layouts/DashboardLayout'
 
 /**
@@ -11,11 +12,12 @@ import DashboardLayout from '@/layouts/DashboardLayout'
  * 주스탠드에 저장된 대시보드 목록에서 dashboardid 로 title 가져올 수 잇을 듯
  */
 export default function UserDashboardLayout({ children }: PropsWithChildren) {
-  const { dashboardid } = useParams()
+  const { id } = useParams()
+  const { title } = getDashboardInfoById(id)
 
   return (
     <DashboardLayout
-      title={`대시보드 타이틀: ${dashboardid}`}
+      title={title}
       className='flex h-full w-full flex-col flex-nowrap lg:flex-row'
     >
       {children}
