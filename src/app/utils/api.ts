3b13370cebaa318a661336bdd10169913537ad93
JiinSeok <seokjiin1073@gmail.com>
2024-08-08 17:59:47 +0900

    [#39] 대시보드 정보, 초대 함수 추가

--- src/app/utils/api.ts
+++ src/app/utils/api.ts
@@ -1,6 +1,19 @@
 import api from '@/app/utils/axiosInstance'
 
 export async function getDashboardInfoById(id: number) {
-  const response = await api.get(`/dashboards/${id}`)
-  return response.data
+  try {
+    const response = await api.get(`/dashboards/${id}`)
+    return response.data
+  } catch (error) {
+    return error.message
+  }
+}
+
+export async function getDashboardInvitationById(id: number) {
+  try {
+    const response = await api.get(`/dashboards/${id}/invitations`)
+    return response.data
+  } catch (error) {
+    return error.message
+  }
 }
