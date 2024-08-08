9b36c329375eb79c49b914470da0d433b546bc12
JiinSeok <seokjiin1073@gmail.com>
2024-08-08 21:31:11 +0900

    [#39] 네트워크 요청 함수 파일

--- src/app/utils/api.ts
+++ src/app/utils/api.ts
@@ -9,6 +9,15 @@ export async function getDashboardInfoById(id: number) {
   }
 }
 
+export async function updateDashboardInfo(id: number, body: any) {
+  try {
+    const response = await api.put(`/dashboards/${id}`, body)
+    return response.data
+  } catch (error) {
+    return error.message
+  }
+}
+
 export async function getDashboardInvitationById(id: number) {
   try {
     const response = await api.get(`/dashboards/${id}/invitations`)
