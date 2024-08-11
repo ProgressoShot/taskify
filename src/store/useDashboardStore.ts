9d3e1a954fcfccda27d40ba1d5d9c6cd419c0682
JiinSeok <seokjiin1073@gmail.com>
2024-08-12 00:24:13 +0900

    [#39] :recycle: 대시보드 상태 리팩토링

--- src/store/useDashboardStore.ts
+++ src/store/useDashboardStore.ts
@@ -1,19 +1,24 @@
 import { create } from 'zustand'
 
-import type {Dashboard} from '@/types/types'
+import type { Dashboard } from '@/types/types'
 
 interface DashboardStore {
+  title: string
+  setTitle: (data: string) => void
   dashboard: Dashboard
   setDashboard: (data: Dashboard) => void
-  dashboardList: Dashboard[] | null
-  setDashboardList: (data: Dashboard[] | null) => void
+  dashboards: Dashboard[] | null
+  setDashboards: (data: Dashboard[] | null) => void
 }
 
 const useDashboardStore = create<DashboardStore>(set => ({
-  dashboard: {},
+  title: '',
+  setTitle: (data: string) => set(state => ({ title: data })),
+  dashboard: {} as Dashboard,
   setDashboard: (data: Dashboard) => set(state => ({ dashboard: data })),
-  dashboardList: null,
-  setDashboardList: (data: Dashboard[] | null) => set(state => ({ dashboardList: data })),
+  dashboards: null,
+  setDashboards: (data: Dashboard[] | null) =>
+    set(state => ({ dashboards: data })),
 }))
 
 export default useDashboardStore
