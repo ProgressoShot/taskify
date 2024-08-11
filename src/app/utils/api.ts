29ea6a76251b2cafb7bc25771d2342d94ad51f44
JiinSeok <seokjiin1073@gmail.com>
2024-08-12 00:16:32 +0900

    [#39] :bug: 빌드 오류 해결

--- src/app/utils/api.ts
+++ src/app/utils/api.ts
@@ -1,46 +1,32 @@
 import api from '@/app/utils/axiosInstance'
+import {Dashboard, Invitations, ListDashboardInvitationsResponse} from '@/types/types'
 
-export async function getDashboardInfo(dashboardId: number) {
+export async function getDashboard(id: number): Promise<Dashboard> {
   try {
-    const response = await api.get(`/dashboards/${dashboardId}`)
-    return response.data
-  } catch (error) {
-    return error.message
-  }
-}
-
-export async function updateDashboardTitle(id: number, title: string) {
-  try {
-    const response = await api.put(`/dashboards/${id}`, {
-      title,
-    })
-    return response.data
-  } catch (error) {
-    return error.message
-  }
-}
-
-export async function updateDashboardInfo(
-  id: number,
-  title: string,
-  color: string
-) {
-  try {
-    const response = await api.put(`/dashboards/${id}`, {
-      title,
-      color,
-    })
+    const response = await api.get(`/dashboards/${id}`)
     return response.data
   } catch (error: any) {
     return error.message
   }
 }
 
-export async function getDashboardInvitationById(id: number) {
+export async function updateDashboard(
+  id: number,
+  body: { title: string; color: string }
+) {
+  try {
+    const response = await api.put(`/dashboards/${id}`, body)
+    return response.data
+  } catch (error: any) {
+    return error.message
+  }
+}
+
+export async function listDashboardInvitations(id: number): Promise<ListDashboardInvitationsResponse> {
   try {
     const response = await api.get(`/dashboards/${id}/invitations`)
     return response.data
-  } catch (error) {
+  } catch (error: any) {
     return error.message
   }
 }
