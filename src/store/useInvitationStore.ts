81eef178acc7efe76a2f996af437af002dc9c778
JiinSeok <seokjiin1073@gmail.com>
2024-08-12 00:24:45 +0900

    [#39] :sparkle: invitation 상태 추가

---
+++ src/store/useInvitationStore.ts
@@ -0,0 +1,23 @@
+import { create } from 'zustand'
+
+import { Invitation, Invitations } from '@/types/types'
+
+interface DashboardInvitaionStore {
+  invitation: Invitation
+  setInvitation: (data: Invitation) => void
+  invitations: Invitations
+  setInvitations: (data: Invitations) => void
+  totalCount: number
+  setTotalCount: (data: number) => void
+}
+
+export const useDashboardInvitationStore = create<DashboardInvitaionStore>(set => ({
+  invitation: {} as Invitation,
+  setInvitation: (data: Invitation) =>
+    set(state => ({ invitation: data })),
+  invitations: [],
+  setInvitations: (data: Invitations) =>
+    set(state => ({ invitations: data })),
+  totalCount: 0,
+  setTotalCount: (data: number) => set(state => ({ totalCount: data })),
+}))
