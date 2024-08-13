341c66a32f49c408b1dd1586d971ed7956ec19b4
JiinSeok <seokjiin1073@gmail.com>
2024-08-14 01:41:37 +0900

    [#171] mock 파일 생성

---
+++ src/lib/mock.ts
@@ -0,0 +1,44 @@
+import {DashboardMember} from "@/types/types";
+
+export const DASHBOARD_MEMBERS: DashboardMember[] = [
+  {
+    id: 1,
+    userId: 1,
+    nickname: '정만철',
+    email: 'test1@test.com',
+    profileImageUrl: '',
+    updatedAt: '2024-08-13T14:52:12.643Z',
+    createdAt: '2024-08-13T14:52:12.643Z',
+    isOwner: true,
+  },
+  {
+    id: 2,
+    userId: 2,
+    nickname: '김태순',
+    email: 'test2@test.com',
+    profileImageUrl: '',
+    updatedAt: '2024-08-13T14:52:12.643Z',
+    createdAt: '2024-08-13T14:52:12.643Z',
+    isOwner: false,
+  },
+  {
+    id: 3,
+    userId: 3,
+    nickname: '최주협',
+    email: 'test3@test.com',
+    profileImageUrl: '',
+    updatedAt: '2024-08-13T14:52:12.643Z',
+    createdAt: '2024-08-13T14:52:12.643Z',
+    isOwner: false,
+  },
+  {
+    id: 4,
+    userId: 4,
+    nickname: '윤지현',
+    email: 'test4@test.com',
+    profileImageUrl: '',
+    updatedAt: '2024-08-13T14:52:12.643Z',
+    createdAt: '2024-08-13T14:52:12.643Z',
+    isOwner: false,
+  },
+]
