9244507f3b35a003b26a68e6748d2abaf88e8826
ylem76 <ylem76@gmail.com>
2024-08-17 15:22:14 +0900

    [#206] axios로 변경

--- src/app/login/loginAction.tsx
+++ src/app/login/loginAction.tsx
@@ -1,5 +1,4 @@
 'use server'
-
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
@@ -10,33 +9,19 @@ import { LoginFormValue } from './components/LoginForm'
 export default async function loginAction(data: LoginFormValue) {
   // 백엔드 요청
   try {
-    const response = await fetch(
-      'https://sp-taskify-api.vercel.app/7-2/auth/login',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      }
-    )
-
-    const responsedData = await response.json()
-    if (responsedData.message) {
-      throw new Error(responsedData.message)
-    }
-    const { accessToken, user } = responsedData
+    const response = await api.post('/auth/login', data, {})
+    const { accessToken, user } = response.data
 
     // 가져온 json token 쿠키 설정 하기
     cookies().set('Authorization', accessToken, {
       secure: true,
       httpOnly: true,
-      expires: Date.now() + 24 * 60 * 60 * 1000 * 3,
+      expires: new Date(Date.now() + 24 * 60 * 60 * 1000 * 3),
       path: '/',
       sameSite: 'strict',
     })
   } catch (error: any) {
-    return error.message
+    return error.response?.data?.message || error.message
   }
 
   // 로그인 여부에 따라 redirect
--- src/lib/axiosInstance.ts
+++ src/lib/axiosInstance.ts
@@ -15,17 +15,17 @@ export const setAccessToken = (token: string) => {
   accessToken = token
 }
 
-api.interceptors.request.use(
-  config => {
-    const token = sessionStorage.getItem('accessToken')
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`
-    }
-    return config
-  },
-  error => {
-    return Promise.reject(error)
-  }
-)
+// api.interceptors.request.use(
+//   config => {
+//     const token = sessionStorage.getItem('accessToken')
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`
+//     }
+//     return config
+//   },
+//   error => {
+//     return Promise.reject(error)
+//   }
+// )
 
 export default api
